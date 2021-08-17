ae8b83c2b35b776a4e9e3f8cfa2fde3ab202c555
Atharva <atharva1525@gmail.com>
2021-08-17 16:35:47 +0530

    Update Customer.java

--- src/main/java/com/project/entities/Customer.java
+++ src/main/java/com/project/entities/Customer.java
@@ -16,24 +16,25 @@ import javax.persistence.Table;
 public class Customer {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int customerId;
+
 	private String email;
 	private String fullName;
 	private String password;
-	
-	
-	@OneToOne(cascade = CascadeType.ALL,targetEntity = Address.class)
-	@JoinColumn(name="addressId")
+
+	@OneToOne(cascade = CascadeType.ALL)
 	private Address address;
 	private String mobileNumber;
 	private LocalDate registerOn;
-	
-	
-	public Customer() {}
-	
+	private String userType;
+
+	public Customer() {
+	}
+
 	public Customer(int customerId, String email, String fullName, String password, Address address,
-			String mobileNumber, LocalDate registerOn) {
+			String mobileNumber, LocalDate registerOn, String userType) {
+		super();
 		this.customerId = customerId;
 		this.email = email;
 		this.fullName = fullName;
@@ -41,54 +42,78 @@ public class Customer {
 		this.address = address;
 		this.mobileNumber = mobileNumber;
 		this.registerOn = registerOn;
+		this.userType = userType;
 	}
+
 	public int getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(int customerId) {
 		this.customerId = customerId;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 	public String getFullName() {
 		return fullName;
 	}
+
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public LocalDate getRegisterOn() {
 		return registerOn;
 	}
+
 	public void setRegisterOn(LocalDate registerOn) {
 		this.registerOn = registerOn;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
 	@Override
 	public String toString() {
 		return "Customer [customerId=" + customerId + ", email=" + email + ", fullName=" + fullName + ", password="
 				+ password + ", address=" + address + ", mobileNumber=" + mobileNumber + ", registerOn=" + registerOn
-				+ "]";
+				+ ", userType=" + userType + "]";
 	}
-	
+
 }
